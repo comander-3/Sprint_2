440c38652c4ac03d743d5872a071a52d3efd09b7
Victor Ershov <comander-3@yandex.ru>
2025-04-09 03:09:04 +0500

    Add task_1-5.py

---
+++ task_3.py
@@ -0,0 +1,44 @@
+class PointsForPlace:
+    points = 0
+        
+    @staticmethod
+    def get_points_for_place(place):
+        if place > 100:
+            return f'Баллы начисляются только первым 100 участникам'
+        elif place < 1:
+            return f'Спортсмен не может занять нулевое или отрицательное место'
+        else:
+            points = 101 - place
+            return points
+
+
+class PointsForMeters:
+    points = 0
+
+    @staticmethod
+    def get_points_for_meters(meters):
+        if meters < 0:
+            return f'Количество метров не может быть отрицательным'
+        else:
+            points = meters * 0.5
+            return points
+
+
+class TotalPoints(PointsForPlace, PointsForMeters):
+    def __init__(self):
+        PointsForPlace.__init__(self)
+        PointsForMeters.__init__(self)    
+
+    
+    def get_total_points(self, meters, place):
+        total = PointsForMeters.get_points_for_meters(meters) + PointsForPlace.get_points_for_place(place)
+        return total
+
+
+print(PointsForPlace.get_points_for_place(10))
+print(PointsForMeters.get_points_for_meters(10))
+
+total_points = TotalPoints()
+print(total_points.get_points_for_place(10))
+print(total_points.get_points_for_meters(10))
+print(total_points.get_total_points(100, 10))
